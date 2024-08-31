9e6685fc4a9b2fffdd22587087e49a7da0c5bf21
sachit <sachitforsocials@gmail.com>
2024-08-31 18:55:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const [length,setLength] = useState(8)
+const [numAllowd,setNumAllowed] = useState(false)
+const [charAllowed,setCharAllowed] = useState(false)
+const [password,setPassword] = useState("")
+
+// now lets make a password generator
+function App() {
+   
+
+  return (
+    <>
+
+     
+    
+ 
+    </>
+  )
+}
+
+export default App
